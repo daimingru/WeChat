746543fef08bdca547d6645cb98f97ee91e901b8
daimingru <daimingru@okay.cn>
2017-08-25 11:34:40 +0800

    修改数据库密码

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,10 +1,10 @@
 <?php
 return array(
   'DB_TYPE'    => 'mysql',// 数据库类型
-  'DB_HOST'    => '127.0.0.1',// 数据库服务器地址
+  'DB_HOST'    => '101.200.62.18',// 数据库服务器地址
   'DB_NAME'    => 'guitar',// 数据库名称
   'DB_USER'    => 'root',// 数据库用户名
-  'DB_PWD'     => 'root',// 数据库密码
+  'DB_PWD'     => 'cc520who',// 数据库密码
   'DB_PREFIX'  => 'guitar_',// 数据表前缀
   'DB_CHARSET' => 'utf8',// 网站编码
   'DB_PORT'    => '3306',// 数据库端口
