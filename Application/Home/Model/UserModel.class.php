1e7d8572a6404df66200a2a6b377c96b432dbd94
daimingru <daimingru@okay.cn>
2017-08-28 18:36:01 +0800

    f

--- Application/Home/Model/UserModel.class.php
+++ Application/Home/Model/UserModel.class.php
@@ -7,10 +7,10 @@
 namespace Home\Model;
 class UserModel extends BaseModel {
 
-  public function saveInfo(){
+  public function saveInfo($userinfo,$code){
       $appID = 'wx4c9e5b7490b9edee';
       $AppSecret = 'd13a53d06c90ea288d90881bae6ce942';
-      $url = 'https://api.weixin.qq.com/sns/jscode2session?appid='.$appID.'&secret='.$AppSecret.'&js_code=0817hSyt08MnPe1s6rwt0ikOyt07hSy6&grant_type=authorization_code';
+      $url = 'https://api.weixin.qq.com/sns/jscode2session?appid='.$appID.'&secret='.$AppSecret.'&js_code='.$code.'&grant_type=authorization_code';
       $data = $this -> getCurl($url);
       return $data;
   }
