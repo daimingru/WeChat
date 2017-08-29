e1bd3195cb25034eb41646bd917c37f009f1cce5
daimingru <daimingru@okay.cn>
2017-08-29 16:21:14 +0800

    d

--- Application/Home/Model/UserModel.class.php
+++ Application/Home/Model/UserModel.class.php
@@ -13,8 +13,8 @@ class UserModel extends BaseModel {
       $url = 'https://api.weixin.qq.com/sns/jscode2session?appid='.$appID.'&secret='.$AppSecret.'&js_code='.$code.'&grant_type=authorization_code';
       $data = $this -> getCurl($url);
       $data = json_decode($data,true);
-      $userinfo = json_decode($userinfo,true);
-      return is_array($userinfo);
+      $userinfo = json_decode($userinfo);
+      return $userinfo;
       exit();
       //$userinfo['openid'] = $data['openid'];
       if($data['openid']){
