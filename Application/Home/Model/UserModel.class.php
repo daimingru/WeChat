4116159ddeaf74cb8332ca4b6e1634c5186b3cf4
daimingru <daimingru@okay.cn>
2017-08-29 14:42:31 +0800

    df

--- Application/Home/Model/UserModel.class.php
+++ Application/Home/Model/UserModel.class.php
@@ -10,16 +10,19 @@ class UserModel extends BaseModel {
   public function saveInfo($userinfo,$code){
       $appID = 'wx4c9e5b7490b9edee';
       $AppSecret = 'd13a53d06c90ea288d90881bae6ce942';
+      $userinfo = '{"nickName":"programmer","gender":1,"language":"zh_CN","city":"Yancheng","province":"Jiangsu","country":"China","avatarUrl":"https://wx.qlogo.cn/mmopen/LjQmhzIQCrFBysibGEohkZicduKPux035cE9wUjsmMoHctG1gCOZcOPztgUe3QLMMUiaLDH0GugOwn6wmR934CibuA/0"}';
       $url = 'https://api.weixin.qq.com/sns/jscode2session?appid='.$appID.'&secret='.$AppSecret.'&js_code='.$code.'&grant_type=authorization_code';
       $data = $this -> getCurl($url);
       $data = json_decode($data,true);
-      $userinfo = json_decode($userinfo);
-      //
-      return $userinfo->{"nickName"};
-      exit();
-      $User = M("user"); // 实例化User对象
-      $userinfo['openid'] = $data['openid'];
-      $User->add($userinfo);
+      $userinfo = json_decode($userinfo,true);
+      $userinfo['openid'] = '"d13a53d06c90ea288d90881bae6ce942s"';
+      $sql  = 'SELECT * FROM __PREFIX__user where openid ='.$userinfo['openid'];
+      $rs 	= $this->query($sql);
+      if(!$rs){
+        $User = M("user"); // 实例化User对象
+        $userinfo['openid'] = $data['openid'];
+        $User->add($userinfo);
+      }
       return $User;
   }
 
