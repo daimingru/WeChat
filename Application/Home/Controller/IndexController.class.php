3b6eaa3339b49e1e9db22e54a13b0cd5f2c7a99b
daimingru <daimingru@okay.cn>
2017-11-29 18:59:52 +0800

    f

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,11 +4,16 @@ use Think\Controller;
 class IndexController extends Controller {
     public function index(){
       $this->assign("selectStatus", "index");
-      $this->display("index/index");
+      $this->display("column/column_y");
     }
 
     //后台登录
     public function admin(){
       $this->display("admin/login/index");
     }
+
+    public function column(){
+      $this->display("column/column_y");
+    }
+
 }
