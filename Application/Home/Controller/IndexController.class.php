15d13ed2554bd9b17f4d422141e94305abc57154
戴明儒 <daimingru@okay.cn>
2018-06-28 14:43:58 +0800

    F

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -6,19 +6,19 @@ use Org\Util\WeChat;
 class IndexController extends Controller {
 
     public function index(){
-
-      $foo = WeChat::getSignPackage();
-      $this->assign("selectStatus", "index");
-      $this->display("index/index");
+        $foo = new WeChat();
+        $WeChat = $foo->getSignPackage('wx7fde6fb244527847','e61d08e0555fdd397d2c2111d685bc18');
+        $this->assign("selectStatus", $WeChat);
+        $this->display("index/index");
     }
 
     //后台登录
     public function admin(){
-      $this->display("admin/login/index");
+        $this->display("admin/login/index");
     }
 
     public function column(){
-      $this->display("column/column_y");
+        $this->display("column/column_y");
     }
 
 }
